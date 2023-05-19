915782c4c5f6907998eec75a6a1197503dc703a5
Dawidw9595 <116345825+Dawidw9595@users.noreply.github.com>
2023-05-19 09:29:13 +0200

    layout ciało

--- app/src/main/java/com/example/rollapp/MainActivity.java
+++ app/src/main/java/com/example/rollapp/MainActivity.java
@@ -186,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, cechyPostaci.class);
+                Intent intent = new Intent(MainActivity.this, umCialoPostaci.class);
                 startActivity(intent);
             }
         });
