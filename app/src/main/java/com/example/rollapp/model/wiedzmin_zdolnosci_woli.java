c112b9157aed93b5849467b38a3c986e6f1cc67c
Dawidw9595 <116345825+Dawidw9595@users.noreply.github.com>
2023-05-21 18:43:24 +0200

    wola controler

---
+++ app/src/main/java/com/example/rollapp/model/wiedzmin_zdolnosci_woli.java
@@ -0,0 +1,98 @@
+package com.example.rollapp.model;
+
+public class wiedzmin_zdolnosci_woli {
+
+    private int id;
+    private int id_karta;
+    private int klatwy;
+    private int lamanie_magii;
+    private int nieugietosc;
+    private int odwaga;
+    private int rytualy;
+    private int zaklecia;
+    private int zastraszanie;
+
+    public wiedzmin_zdolnosci_woli() {
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getKlatwy() {
+        return klatwy;
+    }
+
+    public void setKlatwy(int klatwy) {
+        this.klatwy = klatwy;
+    }
+
+    public int getLamanie_magii() {
+        return lamanie_magii;
+    }
+
+    public void setLamanie_magii(int lamanie_magii) {
+        this.lamanie_magii = lamanie_magii;
+    }
+
+    public int getNieugietosc() {
+        return nieugietosc;
+    }
+
+    public void setNieugietosc(int nieugietosc) {
+        this.nieugietosc = nieugietosc;
+    }
+
+    public int getOdwaga() {
+        return odwaga;
+    }
+
+    public void setOdwaga(int odwaga) {
+        this.odwaga = odwaga;
+    }
+
+    public int getRytualy() {
+        return rytualy;
+    }
+
+    public void setRytualy(int rytualy) {
+        this.rytualy = rytualy;
+    }
+
+    public int getZaklecia() {
+        return zaklecia;
+    }
+
+    public void setZaklecia(int zaklecia) {
+        this.zaklecia = zaklecia;
+    }
+
+    public int getZastraszanie() {
+        return zastraszanie;
+    }
+
+    public void setZastraszanie(int zastraszanie) {
+        this.zastraszanie = zastraszanie;
+    }
+
+    @Override
+    public String toString() {
+        return "wiedzmin_zdolnosci_woli{" +
+                "id=" + id +
+                ", id_karta=" + id_karta +
+                ", klatwy=" + klatwy +
+                ", lamanie_magii=" + lamanie_magii +
+                ", nieugietosc=" + nieugietosc +
+                ", odwaga=" + odwaga +
+                ", rytualy=" + rytualy +
+                ", zaklecia=" + zaklecia +
+                ", zastraszanie=" + zastraszanie +
+                '}';
+    }
+}
+
+
---
+++ app/src/main/java/com/example/rollapp/retrofit/wolaApi.java
@@ -0,0 +1,17 @@
+package com.example.rollapp.retrofit;
+
+import com.example.rollapp.model.wiedzmin_zdolnosci_woli;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface wolaApi {
+    @POST("/wola/save")
+    Call<Void> save(@Body wiedzmin_zdolnosci_woli wiedzmin_zdolnosci_woli);
+
+    @POST("/wola/getall")
+    Call<ArrayList<wiedzmin_zdolnosci_woli>> getall(@Body wiedzmin_zdolnosci_woli wiedzmin_zdolnosci_woli);
+}
