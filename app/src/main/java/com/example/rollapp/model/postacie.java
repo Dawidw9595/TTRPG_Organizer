f8a48895c7134a3c3cd563d851fd6257d2fdeb12
Dawidw9595 <116345825+Dawidw9595@users.noreply.github.com>
2023-05-23 13:55:41 +0200

    layouty do zrobienia, chyba już wszystkie

---
+++ app/src/main/java/com/example/rollapp/model/postacie.java
@@ -0,0 +1,14 @@
+package com.example.rollapp.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class postacie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postacie);
+    }
+}
